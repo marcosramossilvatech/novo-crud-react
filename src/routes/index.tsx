ae1d29de039dc61d8f5fb9e42d7056792178eee5
Marcos <mrsilvatdas@gmail.com>
2023-07-22 01:42:27 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
+
+export const AppRoutes =() =>{
+  const { toggleTheme } = useAppThemeContext();
+
+  return (
+    <Routes>
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Alterar tema
+          </Button>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
